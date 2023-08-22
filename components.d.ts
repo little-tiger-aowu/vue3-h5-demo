cd95ee2c98540165cbbb42b0d57d2a0ad50b6bc1
lzp <58032856+little-tiger-aowu@users.noreply.github.com>
2023-08-22 15:16:20 +0800

    完结

--- components.d.ts
+++ components.d.ts
@@ -9,6 +9,7 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    Music: typeof import('./src/components/music.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     VanButton: typeof import('vant/es')['Button']
